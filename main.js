5ccb2ce9d5b77df157461fd29f52a6a2cacf13f8
krzysztoffila <umowy.fila@gmail.com>
2023-03-03 18:13:36 +0100

    v1

--- main.js
+++ main.js
@@ -7,6 +7,20 @@ document.body.style.backgroundColor = `rgb(${red},${green},${blue})`
 // strzałka w dół - 40, 40
 
 const changeColor = e => {
+  if (e.keyCode === 38) {
+    if (red <= 255) {
+      red += 5
+      green += 5
+      blue += 5
+      document.body.style.backgroundColor = `rgb(${red},${green},${blue})`
+    }
+  } else if (e.keyCode === 40 && red >= 0) {
+    red -= 5
+    green -= 5
+    blue -= 5
+    document.body.style.backgroundColor = `rgb(${red},${green},${blue})`
+  }
+
   console.log(e.keyCode, e.which)
 }
 
