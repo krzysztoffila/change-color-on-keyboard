2b153ba3d22d2bf45d760845009023e677ba139c
krzysztoffila <umowy.fila@gmail.com>
2023-03-04 12:36:19 +0100

    v2 (switch)

--- main.js
+++ main.js
@@ -7,19 +7,29 @@ document.body.style.backgroundColor = `rgb(${red},${green},${blue})`
 // strzałka w dół - 40, 40
 
 const changeColor = e => {
-  if (e.keyCode === 38) {
-    if (red <= 255) {
-      red += 5
-      green += 5
-      blue += 5
-      document.body.style.backgroundColor = `rgb(${red},${green},${blue})`
-    }
-  } else if (e.keyCode === 40 && red >= 0) {
-    red -= 5
-    green -= 5
-    blue -= 5
-    document.body.style.backgroundColor = `rgb(${red},${green},${blue})`
+  // if (e.keyCode === 38) {
+  //   if (red <= 255) {
+  //     red += 5
+  //     green += 5
+  //     blue += 5
+  //     document.body.style.backgroundColor = `rgb(${red},${green},${blue})`
+  //   }
+  // } else if (e.keyCode === 40 && red >= 0) {
+  //   red -= 5
+  //   green -= 5
+  //   blue -= 5
+  //   document.body.style.backgroundColor = `rgb(${red},${green},${blue})`
+  // }
+
+  switch (e.keyCode) {
+    case 38:
+      document.body.style.backgroundColor = `rgb(${red < 255 ? red +=5 : red},${green+=5},${blue+=5})`;
+      break;
+    case 40:
+      document.body.style.backgroundColor = `rgb(${red > 0 ? red -=5 : red},${green > 0 ? green-=5 : green},${blue > 0 ? blue-=5 : blue})`;
+      break;
   }
+
 
   console.log(e.keyCode, e.which)
 }
